dcf08d5c6de881b1d57c806e92ba300c9f69602b
vanle3664 <73935630+vanle3664@users.noreply.github.com>
2021-03-16 16:01:29 +0700

    Update DynamicObject.cpp

--- DynamicObject.cpp
+++ DynamicObject.cpp
@@ -4,5 +4,7 @@ class DynamicObject: public BaseObject{
 	public:
 		DynamicObject(int id, string name, int position) : BaseObject(id, name, position){}
 		~ DynamicObject(){}
-		virtual void move() = 0;
+		virtual void move(){
+			cout << "Move" << endl;
+		}
 };
