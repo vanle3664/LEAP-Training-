cf1f88453bd053a9883fa3f00811e64e9e002b4c
vanle3664 <73935630+vanle3664@users.noreply.github.com>
2021-03-16 16:02:54 +0700

    Update StaticObject.cpp

--- StaticObject.cpp
+++ StaticObject.cpp
@@ -4,4 +4,7 @@ class StaticObject: public BaseObject{
 	public:
 		StaticObject(int id, string name, int position) : BaseObject(id, name, position){}
 		~ StaticObject(){}
+		void move(){
+			cout << "Can't move" << endl;
+		}
 };
