d14d9984d2a2fe31b5966d16fa5aab24a75f9fbd
vanle3664 <73935630+vanle3664@users.noreply.github.com>
2021-03-16 16:01:52 +0700

    Update House.cpp

--- House.cpp
+++ House.cpp
@@ -2,4 +2,7 @@ class House : public StaticObject{
 	public:
 		House(int id, string name, int position) : StaticObject(id, name, position){}
 		~House(){}
+		void move(){
+			cout << "Can't move" << endl;
+		}
 };
