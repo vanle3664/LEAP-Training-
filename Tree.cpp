d20238596942ba531fa64c43773a2f517dab6c82
vanle3664 <73935630+vanle3664@users.noreply.github.com>
2021-03-16 16:03:23 +0700

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -2,5 +2,7 @@ class Tree : public StaticObject{
 	public:
 		Tree(int id, string name, int position) : StaticObject(id, name, position){}
 		~Tree(){}
-		
+		void move(){
+			cout << "Can't move" << endl;
+		}
 };
