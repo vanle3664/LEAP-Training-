4fcae814b97c8fe60b0cd8c7f076889e35572f5b
vanle3664 <73935630+vanle3664@users.noreply.github.com>
2021-03-16 16:10:20 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -9,25 +9,31 @@
 using namespace std;
 
 int main(){
-	list<BaseObject> listObject;
-	//Tree* tree = new Tree(1, "st", 7); 
-	//House* house = new House(2, "nd", 4) ;
-	//Car* car = new Car(3, "rd", 9); 
-	//Motorbike* motorbike = new Motorbike (4, "th", 3);
+	list<BaseObject*> listObject;
+	Tree* tree = new Tree(1, "st", 7); 
+	House* house = new House(2, "nd", 4) ;
+	Car* car = new Car(3, "rd", 9); 
+	Motorbike* motorbike = new Motorbike (4, "th", 3);
 	
-	listObject.push_back(Tree(1, "st", 7));
-	listObject.push_back(House(2, "nd", 4));
-	listObject.push_back(Car(3, "rd", 9));
-	listObject.push_back(Motorbike (4, "th", 3));
+	listObject.push_back(tree);
+	listObject.push_back(house);
+	listObject.push_back(car);
+	listObject.push_back(motorbike);
+	
+	
+//	listObject.push_back(Tree(1, "st", 7));
+//	listObject.push_back(House(2, "nd", 4));
+//	listObject.push_back(Car(3, "rd", 9));
+//	listObject.push_back(Motorbike (4, "th", 3));
 	
 	for (auto obj: listObject){
-		obj.getName(); 
+		obj->getName(); 
 	}
 	for (auto obj: listObject){
-		obj.move(); 
+		obj->move(); 
 	}
 	for (auto obj: listObject){
-		obj.printPosition(); 
+		obj->printPosition(); 
 	}
 	listObject.clear();
 	return 0;
